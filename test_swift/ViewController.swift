e7f0f4b2862a704d6806e0a09e5bbc80c6a301cb
“Taiyo <taitai9847@gmail.com>
2018-08-13 17:31:07 +0900

    test_swift

--- test_swift/ViewController.swift
+++ test_swift/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hoge_button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        hoge_button.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
